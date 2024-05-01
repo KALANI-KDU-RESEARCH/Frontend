20cb5356c288ef9ac6c0fa98c28c03bff801f38d
ThamaliNirmala <tnirmalaadasooriya@gmail.com>
2024-05-01 18:55:33 +0530

    Font changed

--- src/components/eDashBoard.js
+++ src/components/eDashBoard.js
@@ -40,53 +40,87 @@ const EDashBoard = () => {
   }, []);
 
   return (
-    <div className="container">
+    <div className="bg-[#F5F9CD]">
       <Header />
-      <h5 className="">Current Trends Related Your Interests 📈</h5>
-      <div id="widget" className="grid md:grid-cols-2 gap-10 ">
-        {user.interests.map((val) => (
-          <>
-            <GoogleTrends
-              type="TIMESERIES"
-              keyword={val}
-              url="https://ssl.gstatic.com/trends_nrtr/2051_RC11/embed_loader.js"
-            />
-            <GoogleTrends
-              type="GEO_MAP"
-              keyword={val}
-              url="https://ssl.gstatic.com/trends_nrtr/2051_RC11/embed_loader.js"
-            />
-          </>
-        ))}
-      </div>
-      <h5 className="">Recommended Ideas💡</h5>
-      {loading && (
-        <div className="  flex justify-center">
-          <span className=" mt-7">Still Generating &nbsp;&nbsp;</span>
-          <Hearts width="50" color="red" />
+      <div className="container">
+        <h5
+          className="text-[#650D26] text-2xl font-semibold mt-10"
+          style={{ fontFamily: "Montserrat Alternates" }}
+        >
+          Current Trends Related Your Interests 📈
+        </h5>
+        <div id="widget" className="grid md:grid-cols-2 gap-10 ">
+          {user.interests.map((val) => (
+            <>
+              <GoogleTrends
+                type="TIMESERIES"
+                keyword={val}
+                url="https://ssl.gstatic.com/trends_nrtr/2051_RC11/embed_loader.js"
+              />
+              <GoogleTrends
+                type="GEO_MAP"
+                keyword={val}
+                url="https://ssl.gstatic.com/trends_nrtr/2051_RC11/embed_loader.js"
+              />
+            </>
+          ))}
         </div>
-      )}
-      <div className="">
-        {[...dataPredicted].map((element) => (
-          <div className="my-10">
-            <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
-              {element}
-            </Markdown>
-          </div>
-        ))}
-      </div>
-      <div className="">
+        <h5
+          className="text-[#650D26] text-2xl font-semibold mt-10"
+          style={{ fontFamily: "Montserrat Alternates" }}
+        >
+          Recommended Ideas💡
+        </h5>
         {loading && (
-          <InfinitySpin
-            height="200"
-            width="200"
-            radius="9"
-            color="green"
-            ariaLabel="loading"
-            wrapperStyle
-            wrapperClass
-          />
+          <div className="  flex justify-center">
+            <span
+              className=" mt-7"
+              style={{ fontFamily: "Montserrat Alternates" }}
+            >
+              Still Generating &nbsp;&nbsp;
+            </span>
+            <Hearts width="50" color="red" />
+          </div>
         )}
+        <div className="">
+          {[...dataPredicted].map((element) => (
+            <div
+              className="my-10"
+              style={{ fontFamily: "Montserrat Alternates" }}
+            >
+              <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+                {element}
+              </Markdown>
+              <div class="inline-flex items-center justify-center w-full mt-10">
+                <hr class="w-full h-1 my-8 bg-gray-200 border-0 rounded dark:bg-gray-700" />
+                <div class="absolute px-4 -translate-x-1/2 bg-[#F5F9CD] left-1/2 dark:bg-gray-900">
+                  <svg
+                    class="w-4 h-4 text-gray-700 dark:text-gray-300"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 18 14"
+                  >
+                    <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="">
+          {loading && (
+            <InfinitySpin
+              height="200"
+              width="200"
+              radius="9"
+              color="green"
+              ariaLabel="loading"
+              wrapperStyle
+              wrapperClass
+            />
+          )}
+        </div>
       </div>
     </div>
   );
